8bbb68a18778bc89ef6836d2f755e53e92d7c2f7
Laura Kirby <laura.kirby@ibm.com>
2019-02-06 14:46:14 -0800

    clarify steps in analytics requests

--- analytics-api-demo/constants.rb
+++ analytics-api-demo/constants.rb
@@ -1,6 +1,15 @@
-CLIENT_ID = 'Bjq8Z-x7Fg'
-CLIENT_SECRET = 'K8k7sa1O4Wsfz_FLJDJ1VE2-RwheF93v_Ogg_u0CLyJvn7IEBvb-JkfP81ariUDLNWDy_hFSL7net6LL_HwG0oDEMS-uYgSL'
+# 'ENVIRONMENT' should be removed before given to customer for use
 ENVIRONMENT = 'qa.'
+
+# See your 'integration' information within AoC
+CLIENT_ID = 'BNOlkX_7pw'
+CLIENT_SECRET = 'K8k7sa1O4Wsfz_FLJDJ1VE2-RwheF93v_Ogg_u0CLyJvn7IEBvb-JkfP81ariUDLNWDy_hFSL7net6LL_HwG0oDEMS-uYgSL'
+
+# This must be a user's email address who has administrative authority
+USER_EMAIL = 'laurakirby26@gmail.com'
+
+# If you are unsure of your organization's id, see `get_organization.rb` to make the
+# Files API request to obtain your organization details
+# ORGANIZATION_ID is required for Analytics API calls
 ORGANIZATION_ID = 13355
 ORGANIZATION_NAME = 'turbo'
-USER_EMAIL = 'laurakirby26@gmail.com'
--- analytics-api-demo/get_analytics_data.rb
+++ analytics-api-demo/get_analytics_data.rb
@@ -6,22 +6,39 @@ require './authentication.rb'
 require './constants.rb'
 
 include Authentication
-# ------ setup data for Analytics request -------
-analytics_url = "https://api.qa.ibmaspera.com/analytics/v2/organizations/#{ORGANIZATION_ID}/transfers"
-start_time = CGI.escape('2019-01-19T23:00:00Z')
-stop_time = CGI.escape('2019-01-26T23:00:00Z')
-limit = 3
-parameters = "?start_time=#{start_time}&stop_time=#{stop_time}&limit=#{limit}"
-bearer_token = log_in
-puts "\n\nanalytics_url: #{analytics_url + parameters}"
 
-# get page 1 of transfers
-# expect a max of 3 transfers to be returned
+# --------------------------------------
+# Step 1: get authorization
+# --------------------------------------
+# use Authentication module to obtain bearer token
+@bearer_token = log_in
+
+# --------------------------------------
+# Step 2: get page 1 of transfers
+# --------------------------------------
+
+def generate_request_url
+  # analytics base url
+  analytics_url = "https://api.qa.ibmaspera.com/analytics/v2/organizations/#{ORGANIZATION_ID}/"
+  # resource within analytics application
+  analytics_resource = 'transfers'
+  # query parameters
+  start_time = CGI.escape('2019-01-19T23:00:00Z')
+  stop_time = CGI.escape('2019-01-26T23:00:00Z')
+  limit = 3
+  parameters = "?start_time=#{start_time}&stop_time=#{stop_time}&limit=#{limit}"
+  # print what the request url will look like
+  puts "\n\nanalytics_url: #{analytics_url + analytics_resource + parameters}"
+  analytics_url + analytics_resource + parameters
+end
+
+# given our query parameter `limit=3`,
+# expect page one to have a max of 3 transfers returned.
 begin
   puts "\n\nGET Analytics ./transfers page 1\n\n"
   request = RestClient::Resource.new(
-    analytics_url + parameters,
-    headers: { Authorization: bearer_token }
+    generate_request_url,
+    headers: { Authorization: @bearer_token }
   )
 
   result = JSON.parse(request.get, symbolize_names: true)
@@ -30,17 +47,26 @@ rescue Exception => e
   puts e
 end
 
-# get page 2 of transfers
+# --------------------------------------
+# Step 3: get page 2 of transfers
+# --------------------------------------
+
+# given our query parameter `limit=3`,
+# expect page two to have a max of 3 transfers returned.
 begin
   puts "\n\nGET Analytics ./transfers page 2\n\n"
   # link to page two of results is located at `result[:next][:href]`
   if result
+    # any query parameters from initial request will be automatically appended here.
+    # note: result[:first][:href] will always provide the url to the very first page of transfers.
     analytics_url_two = result[:next][:href]
-    # note: result[:first][:href] will always provide the url to the very first page of transfers
+
+    # print what the url will look like for page two of transfers
+    puts "page two url: #{analytics_url_two}"
 
     request_two = RestClient::Resource.new(
       analytics_url_two,
-      headers: { Authorization: bearer_token }
+      headers: { Authorization: @bearer_token }
     )
 
     result_two = JSON.parse(request_two.get, symbolize_names: true)
